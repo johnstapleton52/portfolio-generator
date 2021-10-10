3309c7b3748e5de49e3cdbb87aefac2cde7f4bf7
John Stapleton <johnstapleton52@gmail.com>
2021-10-10 11:41:19 -0700

    Updated work on questions

--- app.js
+++ app.js
@@ -1,14 +1,62 @@
 const inquirer = require('inquirer');
 
-inquirer
+const promptUser = () => {
+return inquirer
   .prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
+
+const promptProject = () => {
+  console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'JQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the Github link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      
     }
   ])
-  .then(answers => console.log(answers));
+}
+ 
+promptUser().then(answers => console.log(answers));
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
 
